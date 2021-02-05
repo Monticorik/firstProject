7d839256923994c7b06b5263ff31c458f639243b
Alexey <example@gmail.com>
2021-02-05 15:21:29 +0200

    Second Practic

--- new project/script.js
+++ new project/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,40 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фиьмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    alert("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы класический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for(let i = 1; i < 3; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    // if (a === null || b === null) {
+    //     alert('Никаких отмен. играем до конца');
+    //     i = 0;
+    //     continue;
+    // } else if (a.length > 50 || b.length > 50) {
+    //     alert('Слишком большое значение');
+    //     i = 0;
+    //     continue;
+    // }  else if (a == "" || b == "") {
+    //     alert('Введите ответ');
+    //     i = 0;
+    //     continue;
+    // }
+
+    if (a != null && b != null && a != "" && b != "" && a.length <50) {
+        personalMovieDB.movies[a] = b; 
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
 
 console.log(personalMovieDB);  
