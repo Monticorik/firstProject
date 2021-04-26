068df4bbf2332793c2715ea60effc41e281211c9
Alexey <example@gmail.com>
2021-04-26 23:51:53 +0300

    Practic #4

--- new project/script.js
+++ new project/script.js
@@ -1,71 +1,68 @@
 "use strict";
 
-let numberOfFilms;
-
-function start () {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-// start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотренно довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log("Вы класический зритель");
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-
-// rememberMyFilms();
-
-function detectPersonalLevel() {
-    for(let i = 1; i < 3; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-        if (a != null && b != null && a != "" && b != "" && a.length <50) {
-            personalMovieDB.movies[a] = b; 
+    privat: true,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотренно довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            console.log("Вы класический зритель");
+        } else if (personalMovieDB.count > 30) {
+            console.log("Вы киноман");
         } else {
-            console.log('error');
-            i--;
+            console.log("Произошла ошибка");
+        }
+    },
+    detectPersonalLevel: function () {
+        for(let i = 1; i < 3; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+            if (a != null && b != null && a != "" && b != "" && a.length <50) {
+                personalMovieDB.movies[a] = b; 
+            } else {
+                console.log('error');
+                i--;
+            }
+            
+        }
+    },
+    toggleVisibleMyDB: function(){
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
         
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log (personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for(let i = 1; i <= 3; i++) {
+            const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (genre == null || genre == "") {
+                console.log("Введите жанр");
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+    
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр #${i+1} - это ${item};`);
+        });
     }
-}
-
-// detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log (personalMovieDB);
-    }
-}
-
-// showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
-
-console.log(document.querySelector(".my-but"));
-
-console.log(personalMovieDB);
+};
--- script.js
+++ script.js
@@ -1,47 +1,4 @@
 "use strict";
 
-let num = 20;
-
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 10;
-    console.log(num);
-}
-
-showFirstMessage("Hello World!");
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-
-
-function ret() {
-    let num = 50;
-    console.log(calc(4, 5));
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-
-let logger = function() {
-    console.log("Hello");
-};
-
-logger();
-
-
-let c = 4;
-function addX(x) {
-  return function(n) {
-     return n + x;
-  }
-}
-const addThree = addX(3);
-let d = addThree(c);
-console.log('example partial application', d);
+let a = prompt("жми отмену");
+console.log(a);
